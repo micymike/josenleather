4b7a5016debd8dc1ed924190407e6c0c58b7046d
micymike <mosesmmichael878@gmail.com>
2025-08-27 09:38:28 +0300

    frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true
   });
 
